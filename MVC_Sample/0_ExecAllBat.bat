1c8e7c4a7d8933f5f87ace1f0d4d4e67b002cfc5
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2020-07-15 20:08:13 +0900

    Adjusting the build system.

--- MVC_Sample/0_ExecAllBat.bat
+++ MVC_Sample/0_ExecAllBat.bat
@@ -5,15 +5,12 @@ echo | call 1_DeleteFile.bat
 timeout 5
 
 rem echo | 3_BuildLibsAtOtherRepos.bat
-rem echo | 3_BuildLibsAtOtherReposInTimeOfDev.bat
+echo | 3_BuildLibsAtOtherReposInTimeOfDev.bat
 
 @echo on
 timeout 5
 
-echo | 6_Build_WSSrv_sample.bat
-echo | 6_Build_WSSrvCore_sample.bat
-echo | 10_ASPNETWebService.bat
-echo | 10_ASPNETWebServiceCore.bat
+echo | 10_Build_WebAppCore_sample.bat
 
 @echo on
 timeout 5
